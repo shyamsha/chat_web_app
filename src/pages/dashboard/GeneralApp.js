6cc94d6ec380d816f911cf30f8c7aa35d1f4faf9
shyamsha <shyamkumar97@live.com>
2025-05-28 18:27:29 +0530

    create chat conversation component

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, useTheme } from "@mui/material";
 import Chats from "./Chats";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
+  const theme = useTheme();
   return (
     <>
       <Stack direction={"row"} sx={{ width: "100%" }}>
+        {/* chats */}
         <Chats />
+        <Box
+          sx={{
+            height: "100%",
+            width: "calc(100vw - 420px)",
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#fff"
+                : theme.palette.background.default,
+          }}
+        >
+          {/* conversations */}
+          <Conversation />
+        </Box>
       </Stack>
     </>
   );
